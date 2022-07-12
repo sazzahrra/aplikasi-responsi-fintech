39f2e596663751189d0c1efc5a5a099861b21d82
salma <sazzahrra@gmail.com>
2022-07-12 13:02:14 +0800

    responsi fintech

---
+++ app/src/main/java/com/example/aplikasi_responsi_fintech/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.example.aplikasi_responsi_fintech
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+        },10000)
+    }
+}
